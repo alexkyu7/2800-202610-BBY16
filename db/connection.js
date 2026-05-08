4e74c7cb5e30b201a90fcd1d5a404a824c3c3258
Samien Munwar <munwarsamien@gmail.com>
2026-05-08 13:20:02 -0700

    Added supabase connection (untested)

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,34 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+// });
+
+// pool.connect((err) => {
+//     if (err) {
+//         console.error('Database connection error:', err.stack);
+//     } else {
+//         console.log('Connected to PostgreSQL database');
+//     }
+// });
+
+// module.exports = pool;
+
+import postgres from "postgres";
+
+const connectionString = process.env.DATABASE_URL;
+const sql = postgres(connectionString);
+
+sql.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err.stack);
+  } else {
+    console.log("Connected to PostgreSQL database");
+  }
 });
 
-pool.connect((err) => {
-    if (err) {
-        console.error('Database connection error:', err.stack);
-    } else {
-        console.log('Connected to PostgreSQL database');
-    }
-});
-
-module.exports = pool;
+export default sql;
